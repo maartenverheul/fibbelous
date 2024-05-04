16889f23db005edd6e9bdba6629228de64e5b6e7
Maarten Verheul <43140847+maartenverheul@users.noreply.github.com>
2024-05-04 21:45:12 +0200

    Init shadcn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold">App</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
