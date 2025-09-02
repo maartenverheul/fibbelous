f9249928f11895c432613563fbb5896ff709a3bd
Maarten Verheul <43140847+maartenverheul@users.noreply.github.com>
2025-09-02 11:46:07 +0200

    Change WorkspaceSelector style

--- react/src/components/sidebar/Sidebar.tsx
+++ react/src/components/sidebar/Sidebar.tsx
@@ -1,38 +1,13 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useNavigate } from "react-router";
-import { useWorkspaceManager } from "@/contexts/WorkspaceManagerContext";
 import TOC from "./TOC";
 import { useTOCContext } from "@/contexts/TOCContext";
 import { usePageManager } from "@/contexts/PageManagerContext";
 import { useAppNavigation } from "@/contexts/AppNavigationContext";
-import { useWorkspace } from "@/contexts/WorkspaceContext";
+import WorkspaceSelector from "./WorkspaceSelector";
 
 export default function Sidebar() {
-  const navigate = useNavigate();
-  const {
-    list: workspaces,
-    selectedWorkspaceId,
-    switchWorkspace,
-  } = useWorkspaceManager();
-  const workspace = useWorkspace();
   const { openPage } = useAppNavigation();
   const { createPage, deletePage } = usePageManager();
   const { toc, loadTOC } = useTOCContext();
-
-  function changeWorkspace(id: string) {
-    if (id === "$manage") navigate("#settings/workspaces");
-    else {
-      const target = workspaces.find((w) => w.id === id);
-      switchWorkspace(id);
-      navigate(`/${target?.slug}`);
-    }
-  }
 
   function handlePageClick(id: string) {
     openPage(id);
@@ -48,24 +23,9 @@ export default function Sidebar() {
 
   return (
     <div className="bg-gray-800 h-full w-full">
-      <Select value={workspace?.id} onValueChange={changeWorkspace}>
-        <SelectTrigger className="text-white rounded-none border-0 border-b select-none w-full text-center mx-auto text-lg !h-12">
-          <SelectValue
-            placeholder="Select an option"
-            className="text-center mx-auto"
-          />
-        </SelectTrigger>
-        <SelectContent>
-          {workspaces.map((workspace) => (
-            <SelectItem key={workspace.id} value={workspace.id}>
-              {workspace.icon} {workspace.title}
-            </SelectItem>
-          ))}
-          <SelectItem value="$manage" className="font-bold">
-            Manage
-          </SelectItem>
-        </SelectContent>
-      </Select>
+      <div className="p-1">
+        <WorkspaceSelector />
+      </div>
 
       <TOC
         className="mt-2"
---
+++ react/src/components/sidebar/WorkspaceSelector.tsx
@@ -0,0 +1,52 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Link, useNavigate } from "react-router";
+import { useWorkspaceManager } from "@/contexts/WorkspaceManagerContext";
+import { useWorkspace } from "@/contexts/WorkspaceContext";
+
+export default function WorkspaceSelector() {
+  const navigate = useNavigate();
+  const { list: workspaces, switchWorkspace } = useWorkspaceManager();
+  const workspace = useWorkspace();
+
+  function changeWorkspace(id: string) {
+    if (id === "$manage") navigate("#settings/workspaces");
+    else {
+      const target = workspaces.find((w) => w.id === id);
+      switchWorkspace(id);
+      navigate(`/${target?.slug}`);
+    }
+  }
+
+  return (
+    <Select value={workspace?.id} onValueChange={changeWorkspace}>
+      <SelectTrigger className="text-white bg-gray-700 border !border-gray-900 rounded-sm select-none w-full text-center mx-auto text-lg !h-12">
+        <SelectValue
+          placeholder="Select an option"
+          className="text-center mx-auto"
+        />
+      </SelectTrigger>
+      <SelectContent>
+        {workspaces.map((workspace) => (
+          <Link to={`/${workspace.slug}`} key={workspace.id}>
+            <SelectItem
+              key={workspace.id}
+              value={workspace.id}
+              className="cursor-pointer"
+            >
+              {workspace.icon} {workspace.title}
+            </SelectItem>
+          </Link>
+        ))}
+        <SelectItem value="$manage" className="font-bold cursor-pointer">
+          Manage
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
